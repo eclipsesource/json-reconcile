c69bf293d3a9223a4fb516272360610dc14d7f89
julia <julia.sme27@gmail.com>
2024-12-13 13:52:47 +0100

    simlify diff model return type, add json ref for conflicts references to the differences

--- src/interfaces/diffmodel.ts
+++ src/interfaces/diffmodel.ts
@@ -1,3 +1,5 @@
+import { DifferenceOperationKind } from "./util.js";
+
 export interface Ressource {
   uri: string;
   diagram: unknown; // ERessource
@@ -5,37 +7,29 @@ export interface Ressource {
 
 export interface DiffModel {
   threeWay: boolean;
-  differences: Difference[];
+  differencesL: Difference[];
+  differencesR: Difference[];
   conflicts: {
-    leftDiffId: number;
-    rightDiffId: number;
+    leftDiff: {
+      $ref: string;
+    };
+    rightDiff: {
+      $ref: string;
+    };
   }[];
 }
 
 export interface Difference {
   id: number;
-  kind: DifferenceKind;
-  source: DifferenceSource;
+  kind: DifferenceOperationKind;
   state: DifferenceState;
   //  + match (sonst weiß Client nicht welche Elemente es visualisieren soll) -> welche Elemente sind gleich, nach Namen oder nach id, oder nach einfachen Pfad
   // frameworks nochmal checken wegen dem matchen - functions die man angeben kann um gleiche Elemente zu identifizieren
   path: string; // additionally added to the copied EMFCompare type
 }
 
-export enum DifferenceKind {
-  ADD = "add",
-  DELETE = "remove",
-  CHANGE = "replace", // or update?
-  MOVE = "move",
-}
-
 export enum DifferenceState {
   UNRESOLVED,
   MERGED,
   DISCARDED,
-}
-
-export enum DifferenceSource {
-  LEFT,
-  RIGHT,
 }
