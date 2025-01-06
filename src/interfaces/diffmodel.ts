6a41eb29b008e5889dcb7e366fab548806b4b598
julia <julia.sme27@gmail.com>
2025-01-06 12:17:56 +0100

    add comment for more clarification for diffmodel differences path

--- src/interfaces/diffmodel.ts
+++ src/interfaces/diffmodel.ts
@@ -25,7 +25,7 @@ export interface Difference {
   state: DifferenceState;
   //  + match (sonst weiß Client nicht welche Elemente es visualisieren soll) -> welche Elemente sind gleich, nach Namen oder nach id, oder nach einfachen Pfad
   // frameworks nochmal checken wegen dem matchen - functions die man angeben kann um gleiche Elemente zu identifizieren
-  path: string; // additionally added to the copied EMFCompare type
+  path: string; // JSON ref path, additionally added to the copied EMFCompare type
 }
 
 export enum DifferenceState {
