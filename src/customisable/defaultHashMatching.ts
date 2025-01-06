d6ac8dcb06bf581bb3a7f4c6fe7fd9c3dcd0b637
julia <julia.sme27@gmail.com>
2025-01-06 12:19:19 +0100

    hash matching, when is an object the same -> take from config

--- src/customisable/defaultHashMatching.ts
+++ src/customisable/defaultHashMatching.ts
@@ -1,6 +1,8 @@
+import { CONFIG } from "../config.js";
+
 export default function defaultHashMatching(
   object: Record<string, string>
 ): string {
   // return objRecord.name || objRecord.id || objRecord._id;
-  return object.id;
+  return object[CONFIG.IDENTIFIER];
 }
