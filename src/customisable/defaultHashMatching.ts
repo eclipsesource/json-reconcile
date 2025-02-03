3ad0eee073762915ed629db4a66c8e253ad072c5
julia <julia.sme27@gmail.com>
2025-02-03 12:00:33 +0100

    fix types for hash matching function

--- src/customisable/defaultHashMatching.ts
+++ src/customisable/defaultHashMatching.ts
@@ -1,8 +1,8 @@
 import { CONFIG } from "../config.js";
 
 export default function defaultHashMatching(
-  object: Record<string, string>
-): string {
+  object: Record<string, undefined>
+): undefined {
   // return objRecord.name || objRecord.id || objRecord._id;
   return object[CONFIG.IDENTIFIER];
 }
