1939de5dc08c907a3396c81ca5c729e9a8b5e755
juu <julia.sme27@gmail.com>
2026-07-18 22:50:13 +0200

    more trial and error code for emf compare models conversion and performance testing of these models

--- performance/src/performanceTesting.ts
+++ performance/src/performanceTesting.ts
@@ -1,9 +1,11 @@
-import fs from "fs";
 import path, { dirname } from "path";
-import { createDiff } from "../../src/services/createDiff.js";
 import { InputModels } from "../../src/interfaces/inputmodels.js";
 import { fileURLToPath } from "url";
-import { convertAllXML, getFile } from "./utils.js";
+import { convertAllXML, writeJson } from "./utils.js";
+import { resolveAllRefs2_bunde, resolveAllRefs_Sol1 } from "../../src/utils/refHandler.js";
+import { JSONValue } from "../../src/utils/jsonHelper.js";
+import * as comparissonMergingService from "../../src/services/comparisonMerging.service.js";
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -19,14 +21,17 @@ enum Side {
   ORIGINAL = "original",
 }
 
+const selectedSize = Size.SMALL;
+const selectedSide = Side.ORIGINAL;
+
 const xmlFolderPath = path.join(
   __dirname,
-  `../data/EMFCompare_Models/model_size_${Size.LARGE}/${Side.ORIGINAL}`,
+  `../data/EMFCompare_Models/model_size_${selectedSize}/${selectedSide}`,
 );
 
 const jsonFolderPath = path.join(
   __dirname,
-  `../data/${Size.LARGE}/${Side.ORIGINAL}`,
+  `../data/${selectedSize}/${selectedSide}`,
 );
 
 function measure<T>(name: string, fn: () => T): T {
@@ -36,23 +41,48 @@ function measure<T>(name: string, fn: () => T): T {
   return result;
 }
 
-function runPerformanceTest(size: string) {
+function runPerformanceTest(size: Size) {
   const fullPath = path.join(__dirname, `../data/${size}`);
   const modelFileName = "model.json";
 
-  const inputModels: InputModels = {
-    left: getFile(`${fullPath}/original`, modelFileName),
-    right: getFile(`${fullPath}/A`, modelFileName),
-  };
+  console.time("resolveRefs");
+  Promise.all([
+    resolveAllRefs2_bunde(
+      `${fullPath}/${Side.ORIGINAL}`,
+      `${fullPath}/${Side.ORIGINAL}/${modelFileName}`,
+    ),
+    resolveAllRefs2_bunde(
+      `${fullPath}/${Side.MODIFIED}`,
+      `${fullPath}/${Side.MODIFIED}/${modelFileName}`,
+    ),
+  ])
+    .then((res) => {
+      console.timeEnd("resolveRefs");
 
-  const diffmodel = measure("Small Model", () => {
-    const result = createDiff(inputModels);
-    return result;
-  });
+      const inputModels: InputModels = {
+        left: res[0] as JSONValue,
+        right: res[1] as JSONValue,
+      };
 
-  console.log(diffmodel);
+      writeJson(fullPath, JSON.stringify(inputModels.left, null, 2), 'original_ref');
+      writeJson(fullPath, JSON.stringify(inputModels.right, null, 2), 'modified_ref');
+
+      const diffmodel = measure(`${size} MODEL - Diff`, () => {
+        const result = comparissonMergingService.createDiff(inputModels);
+        return result;
+      });
+
+      // console.log(diffmodel.differencesL, null, 2)
+      console.log(diffmodel.differencesL.length);
+      console.log(diffmodel.differencesR.length);
+    })
+    .catch((err) => console.log(err.stack));
 }
 
-convertAllXML(xmlFolderPath, jsonFolderPath);
+const args = process.argv.slice(2);
 
-// runPerformanceTest('small');
+if (args.length > 0 && args[0] === "convert") {
+  convertAllXML(xmlFolderPath, jsonFolderPath);
+} else {
+  runPerformanceTest(selectedSize);
+}
