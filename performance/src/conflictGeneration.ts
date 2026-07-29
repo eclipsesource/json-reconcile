c2e73035d6ae2683b1fb49dc10acff06fec628c1
juu <julia.sme27@gmail.com>
2026-07-29 21:50:06 +0200

    further implementation of conflict generation for performance tests

--- performance/src/conflictGeneration.ts
+++ performance/src/conflictGeneration.ts
@@ -3,33 +3,53 @@ import { fileURLToPath } from "url";
 import fs from "fs";
 import seedrandom from 'seedrandom';
 import { JSONValue } from "../../src/utils/jsonHelper.js";
-import { pathFromPtr } from "json-refs";
+import { pathFromPtr, pathToPtr } from "json-refs";
 import { writeJson } from "./utils.js";
 import * as comparissonMergingService from "../../src/services/comparisonMerging.service.js";
 import { InputModels } from "../../src/interfaces/inputmodels.js";
+import { CONFIG } from "../../src/config.js";
 
 
-const rng = seedrandom('tt');
+const rng = seedrandom('juuu');
+
+let base: JSONValue = {}
+let leftSide: Record<string, unknown> = {}
+let rightSide: Record<string, unknown> = {}
+
+const pathsChanged: string[] = [];
 
 function generateConflicts() {
 
-  const NUMBER_OF_CONFLICTS = 1
+  const NUMBER_OF_CONFLICTS = 100
 
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
   const fullPath = path.join(__dirname, '../data/1_MEINS');
   const baseFileName = 'base.json'
 
-  const base = getModel(path.join(fullPath, baseFileName));
+  base = getModel(path.join(fullPath, baseFileName));
   
-  const leftSide = JSON.parse(JSON.stringify(base)) as Record<string, unknown>;
-  const rightSide = JSON.parse(JSON.stringify(base)) as Record<string, unknown>;
+  leftSide = JSON.parse(JSON.stringify(base)) as Record<string, unknown>;
+  rightSide = JSON.parse(JSON.stringify(base)) as Record<string, unknown>;
 
   for (let i = 1; i <= NUMBER_OF_CONFLICTS; i++) {
 
-    // generateUpdateUpdateConflict(base, leftSide, rightSide);
+    // TODO double check if this is legit !!
+    const pick = Math.floor(rng() * 6);
 
-    generateDeleteUpdateConflict(base, leftSide, rightSide);
+    if (pick === 0) {
+      generateDeleteUpdateConflict();
+    } else if (pick === 1) {
+      generateUpdateUpdateConflict()
+    } else if (pick === 2) {
+      generateDeleteUseConflict()
+    } else if (pick === 3) {
+      generateDeleteMoveSourceConflict()
+    } else if (pick === 4) {
+      generateDeleteMoveTargetConflict()
+    } else {
+      generateMoveMoveConflict()
+    }
   }
   
   writeJson(fullPath, JSON.stringify(leftSide, null, 2), 'left');
@@ -43,7 +63,6 @@ function generateConflicts() {
 
   const diffmodel = measure(`${fullPath} MODEL - Diff`, () => {
     const result = comparissonMergingService.createDiff(inputModels);
-    // console.log(JSON.stringify((result)))
     return result;
   });
 
@@ -52,74 +71,85 @@ function generateConflicts() {
 
 // -------------------------------------------------
 
-function generateDeleteUpdateConflict(base: JSONValue, leftSide: Record<string, unknown>, rightSide: Record<string, unknown>) {
+function generateDeleteUpdateConflict() {
   const pathToChange = getRandomNamePath(base);
 
-  const pathToDelete = pathToChange.slice(undefined, -1);
+  const pathToDelete = pathToChange.slice(0, -1);
 
   const valueToChange = getValue(base as unknown as Record<string, unknown>, pathToChange)
 
-  setValue(leftSide, pathToChange, valueToChange + '_ttt');
+  const changeValue = '_' + (Math.floor(rng() * (999 - 100 + 1)) + 100).toString();
+
+  updateValue(leftSide, pathToChange, valueToChange + changeValue);
 
   removeValue(rightSide, pathToDelete);
-
-  // OR more complex:
-
-  // on one side make sure every ref including this path full or starts with this path, these are deleted
-  // then deelte this path
-
-  // on the other side insert an object at this path
-  // maybe get random sub path, starting with the deleted path
 }
 
 
-function generateUpdateUpdateConflict(base: JSONValue, leftSide: Record<string, unknown>, rightSide: Record<string, unknown>) {
+function generateUpdateUpdateConflict() {
   const pathToChange = getRandomNamePath(base);
 
   const valueToChange = getValue(base as unknown as Record<string, unknown>, pathToChange)
 
-  setValue(leftSide, pathToChange, valueToChange + '_ttt')
-  setValue(rightSide, pathToChange, valueToChange + '_zzz')
-}
+  const changeValueLeft = '_' + (Math.floor(rng() * (999 - 100 + 1)) + 100).toString();
+  const changeValueRight = '_' + (Math.floor(rng() * (999 - 100 + 1)) + 100).toString();
 
-// special case: delete - insert use
-function generateDeleteUseConflict(base: JSONValue, leftSide: Record<string, unknown>, rightSide: Record<string, unknown>) {
-  // get a path of an array
-
-  // insert object at the last position
-
-  // reference this object somewhere in the json -> object that has no ref yet
-
-  // OR reference in the same array in an object that has no ref yet
+  updateValue(leftSide, pathToChange, valueToChange + changeValueLeft)
+  updateValue(rightSide, pathToChange, valueToChange + changeValueRight)
 }
 
 
-function generateDeleteMoveConflict(base: JSONValue, leftSide: Record<string, unknown>, rightSide: Record<string, unknown>) {
-  // get a ref
-
-  // ref points to array index normaly
-
-  // if index is 1 switch to 2
-
-  // if index > 1 then switch to index-1
-
-  // change the ref link
-
-  // ON THE OTHER SIDE: delete the newly referenced object
-}
-
-
-function generateMoveMoveConflict(base: JSONValue, leftSide: Record<string, unknown>, rightSide: Record<string, unknown>) {
-  // similar to delete move
-
-  // get a ref
-  // ref points to array index normaly
+function generateDeleteUseConflict() {
+  const objectPathToRef = getRandomObjectPath(base);
   
-  // filter all where the ref index is in array length > (found index + 3)
+  const objectPathToDelete = getRandomObjectPath(base)
 
-  // ON THE ONE SIDE switch to index + 1
+  addKeyValue(leftSide, objectPathToDelete, '$ref', objectPathToRef)
 
-  // ON THE OTHER SIDE switch to index + 2
+  removeValue(rightSide, objectPathToDelete);
+}
+
+
+function generateDeleteMoveSourceConflict() {
+  const objectPath = getRandomObjectPath(base);
+  const objectValue = getValue(leftSide, objectPath);
+
+  const arrayPathLeft = getRandomArrayPath(base);
+
+  removeValue(leftSide, objectPath)
+  addValue(leftSide, arrayPathLeft, objectValue);
+
+  removeValue(rightSide, objectPath);
+}
+
+
+function generateDeleteMoveTargetConflict() {
+  const objectPath = getRandomObjectPath(base);
+  const objectValue = getValue(leftSide, objectPath);
+
+  const arrayPathLeft = getRandomArrayPath(base);
+
+  removeValue(leftSide, objectPath)
+  addValue(leftSide, arrayPathLeft, objectValue);
+
+  removeValue(rightSide, arrayPathLeft);
+}
+
+
+function generateMoveMoveConflict() {
+  const objectPath = getRandomObjectPath(base);
+  const objectValue = getValue(leftSide, objectPath);
+
+  const arrayPathLeft = getRandomArrayPath(base);
+
+  const arrayPathRight = getRandomArrayPath(base);
+
+  removeValue(leftSide, objectPath)
+  addValue(leftSide, arrayPathLeft, objectValue);
+
+  removeValue(rightSide, objectPath);
+  console.log(pathsChanged);
+  addValue(rightSide, arrayPathRight, objectValue);
 }
 
 
@@ -134,31 +164,50 @@ function getModel(fullPath: string): JSONValue | null {
   return JSON.parse(fileContent);
 }
 
+// -------------------------------------------------
+
 // return path
 function getRandomNamePath(json: JSONValue): string[] {
 
   const flatBase = flatten(json).filter((path) => path.includes('name'));
 
-  // console.log(flatBase)
-
   const tempElem = flatBase[getRandomInt(flatBase.length)];
 
-  console.log(tempElem)
+  pathsChanged.push(tempElem);
 
   return pathFromPtr(tempElem)
 }
 
 function getRandomObjectPath(json: JSONValue): string[] {
 
-  const flatBase = flatten(json).filter((path) => /[0-9]$/.test(path));
+  const flatBase = flatten(json).filter((path) => path.endsWith('name'));
 
-  // console.log(flatBase)
+  const objectPaths = flatBase.map((val) => val = val.slice(0, -6));
 
-  const tempElem = flatBase[getRandomInt(flatBase.length)];
+  const randomObjectPointer = objectPaths[getRandomInt(objectPaths.length)];
 
-  console.log(tempElem)
+  pathsChanged.push(randomObjectPointer);
 
-  return pathFromPtr(tempElem)
+  return pathFromPtr(randomObjectPointer)
+}
+
+/* function getRandomRefPath(json: JSONValue): string[] {
+  const flatBase = flatten(json).filter((path) => path.endsWith('ref'));
+
+  const randomRefPointer = flatBase[getRandomInt(flatBase.length)];
+
+  return pathFromPtr(randomRefPointer)
+} */
+
+function getRandomArrayPath(json: JSONValue): string[] {
+
+  const flatBase = flatten(json).filter((path) => path.endsWith('name'));
+
+  const randomPointer = pathFromPtr(flatBase[getRandomInt(flatBase.length)]);
+
+  pathsChanged.push(pathToPtr(randomPointer.slice(0, -2)));
+
+  return randomPointer.slice(0, -2);
 }
 
 function getRandomInt(max: number): number {
@@ -185,6 +234,8 @@ function flatten(obj: JSONValue, pointer = '#', result: string[] = []): string[]
   return result
 }
 
+// -------------------------------------------------
+
 function getValue(json: Record<string, unknown>, path: string[]) {
   const tmp = json[path[0]]
 
@@ -195,7 +246,7 @@ function getValue(json: Record<string, unknown>, path: string[]) {
   return tmp;
 }
 
-function setValue(json: Record<string, unknown>, path: string[], newValue: unknown) {
+function updateValue(json: Record<string, unknown>, path: string[], newValue: unknown) {
   const key = path[0]
 
   if (path.length === 1) {
@@ -203,11 +254,11 @@ function setValue(json: Record<string, unknown>, path: string[], newValue: unkno
     return
   }
 
-  setValue(json[key] as Record<string, unknown>, path.slice(1), newValue)
+  updateValue(json[key] as Record<string, unknown>, path.slice(1), newValue)
 }
 
 function removeValue(json: Record<string, unknown> | unknown[], path: string[]): void {
-  const key = path[0];
+  const key = path[0] as keyof typeof json;
 
   if (path.length === 1) {
     if (Array.isArray(json)) {
@@ -218,7 +269,44 @@ function removeValue(json: Record<string, unknown> | unknown[], path: string[]):
     return;
   }
 
-  removeValue(json[key as keyof typeof json] as Record<string, unknown> | unknown[], path.slice(1));
+  removeValue(json[key] as Record<string, unknown> | unknown[], path.slice(1));
+}
+
+function addValue(json: Record<string, unknown> | unknown[], path: string[], newValue: unknown) {
+  const key = path[0] as keyof typeof json;
+
+  if (path.length === 1) {
+
+    if (!Array.isArray(json[key])) {
+      throw new Error("Parent is not an array");
+    } else {
+      json[key].push(newValue);
+      return ;
+    }
+  }
+
+  console.log(Array.isArray(json), key)
+
+  const newJson = (Array.isArray(json) ? json[parseInt(key)] : (json as Record<string, unknown>)[key]) as Record<string, unknown> | unknown[]
+  addValue(newJson, path.slice(1), newValue);
+}
+
+function addKeyValue(json: Record<string, unknown> | unknown[], path: string[], newKey: string, newValue: unknown): void {
+  const key = path[0] as keyof typeof json;
+
+  if (path.length === 1) {
+
+    if (Array.isArray(json[key])) {
+      throw new Error("Parent is an array");
+    }
+
+    (json[key] as Record<string, unknown>)[newKey] = newValue;
+    return;
+  }
+
+  const newJson = (Array.isArray(json) ? json[Number(key)] : (json as Record<string, unknown>)[key]) as Record<string, unknown> | unknown[]
+
+  const newPath = addKeyValue(newJson, path.slice(1), newKey, newValue);
 }
 
 function measure<T>(name: string, fn: () => T): T {
